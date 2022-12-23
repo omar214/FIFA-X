d835e0f5173b0b6906535cf1da33a5d4476e7d8d
omar214 <omarmohamed2322000@gmail.com>
2022-12-24 01:57:58 +0200

    edit

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,3 @@
 export { default as Navbar } from './MyNavbar.jsx';
 export { default as PaypalCheckoutButton } from './PaypalCheckoutButton.jsx';
 export { default as addStadiumModal } from './AddStadiumModal.jsx';
-export { default as CrossFade } from './CrossFade.jsx';
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,13 +2,11 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Alert from 'react-bootstrap/Alert';
-import data from '../data/data.js';
 import { useEffect, useState } from 'react';
 import API from '../api/api.js';
 import CircularProgress from '@mui/material/CircularProgress';
 import { toast } from 'react-toastify';
 import Carousel from 'react-bootstrap/Carousel';
-import CrossFade from '../components/CrossFade.jsx';
 
 const Home = () => {
 	return (
